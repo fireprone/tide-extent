92d3efb36af9581ecae9bd6c3b29f4d10c6a8f41
Eric Feuerstein <esteinsci@gmail.com>
2022-05-29 12:46:46 -0700

    Create gauge and pointer components

---
+++ src/components/Gauge/Gauge.jsx
@@ -0,0 +1,12 @@
+import './Gauge.css';
+import Pointer from './../Pointer/Pointer';
+
+const Gauge = () => {
+  return (
+    <div id='Gauge'>
+      <Pointer />
+    </div>
+  );
+};
+
+export default Gauge;
---
+++ src/components/Pointer/Pointer.jsx
@@ -0,0 +1,7 @@
+import './Pointer.css';
+
+const Pointer = () => {
+  return <div id='Pointer'></div>;
+};
+
+export default Pointer;
