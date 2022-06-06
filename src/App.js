cac1febb5d8a08d5c2ea3d8b1f7b4812ae5ea3b3
Eric Feuerstein <esteinsci@gmail.com>
2022-06-05 17:55:40 -0700

    Implement scoreboards and turn order

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import { useState } from 'react';
 import './App.css';
 import Gauge from './components/Gauge/Gauge';
+import Scoreboard from './components/Scoreboard/Scoreboard';
 
 function App() {
-  const [points, setPoints] = useState(0);
   const [pointerAngle, setPointerAngle] = useState(0);
   const [targetAngle, setTargetAngle] = useState(0);
+  const [pinkScore, setPinkScore] = useState(0);
+  const [blueScore, setBlueScore] = useState(0);
+  const [turn, setTurn] = useState('pink');
+  let points, setPoints;
 
-  const incrementPoints = (amount) => setPoints(points + amount);
+  if (turn === 'pink') {
+    points = pinkScore;
+    setPoints = setPinkScore;
+  } else {
+    points = blueScore;
+    setPoints = setBlueScore;
+  }
 
-  const withinRange = (distance) =>
+  const incrementPointsBy = (amount) => setPoints(points + amount);
+
+  const withinRangeOf = (distance) =>
     pointerAngle > targetAngle - distance &&
     pointerAngle < targetAngle + distance;
 
   const verifyGuess = () => {
-    if (withinRange(0.1)) {
-      incrementPoints(2);
-    } else if (withinRange(0.275)) {
-      incrementPoints(1);
+    if (withinRangeOf(0.1)) {
+      incrementPointsBy(2);
+    } else if (withinRangeOf(0.275)) {
+      incrementPointsBy(1);
     }
-  };
 
-  console.log(points);
+    setTurn(turn === 'pink' ? 'blue' : 'pink');
+  };
 
   return (
     <div className='App'>
+      <Scoreboard score={pinkScore} color={'lightpink'} align={'left'} />
+      <Scoreboard score={blueScore} color={'lightblue'} align={'right'} />
       <Gauge
         pointerAngle={pointerAngle}
         setPointerAngle={setPointerAngle}
